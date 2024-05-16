0f9fd86f01e6f5f7bbd186f7c061ed12226e93bd
KNA <knayad@gmail.com>
2024-05-16 02:40:01 -0400

    HashRouter; delete extra React files/ code; Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <h1 className="text-red-500 text-5xl">Hello World!</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
