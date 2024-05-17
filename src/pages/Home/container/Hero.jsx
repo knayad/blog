6f8401d91022d117cd85b91c10da69f1fdda2875
KNA <knayad@gmail.com>
2024-05-17 13:12:43 -0400

    xl breakpoints Hero fix

--- src/pages/Home/container/Hero.jsx
+++ src/pages/Home/container/Hero.jsx
@@ -7,14 +7,14 @@ const Hero = () => {
   return (
     <section className="container mx-auto flex flex-col px-5 py-5 lg:flex-row">
       <div className="mt-10 lg:w-1/2">
-        <h1 className="font-sansita-san-serif text-3x text-center font-bold italic text-primary md:text-5xl lg:text-left lg:max-w--[540px]">
+        <h1 className="font-sansita-san-serif text-3x text-center font-bold italic text-primary md:text-5xl lg:text-4xl xl:text-5xl lg:text-left lg:max-w--[540px]">
           Read and write reviews
         </h1>
-        <p className="mt-4 text-center md:text-xl lg:text-left">
+        <p className="mt-4 text-center md:text-xl lg:text-base xl:text-xl lg:text-left">
           A noun refers to a person, place, or thing. Here at Noun, we create a
           centralized space to read and write reviews on any type of noun!
         </p>
-        <div className="flex flex-col gap-y-2.5 mt-10 relative">
+        <div className="flex flex-col gap-y-2.5 mt-10 lg:mt-6 xl:mt-10 relative">
           <div className="relative">
             <FcSearch className="absolute left-3 top-1/2 -translate-y-1/2 w-6 h-6 " />
             <input
@@ -28,11 +28,11 @@ const Hero = () => {
           </button>
         </div>
         <div className="flex mt-4 flex-col lg:flex-row lg:items-start lg:flex-nowrap lg:gap-x-4 lg:mt-7">
-          <span className="text-[#959EAD] font-semibold italic mt-2 lg:mt-4">
+          <span className="text-[#959EAD] font-semibold italic mt-2 lg:mt-4 lg:text-sm xl:text-base">
             {" "}
             Popular tags:{" "}
           </span>
-          <ul className="flex flex-wrap gap-x-2.5 gap-yp2.5 mt-3">
+          <ul className="flex flex-wrap gap-x-2.5 gap-yp2.5 mt-3 lg:text-sm xl:text-base">
             <li className="rounded-lg bg-neon-green bg-opacity-10 px-3 py-1.5 text-neon-green font-semibold italic">
               Person
             </li>
