2597e1c8022a23b4b00149ea0d8b846de03c7ccd
KNA <knayad@gmail.com>
2024-05-19 11:38:01 -0400

    remove one duplicate article

--- src/pages/Home/containers/Articles.jsx
+++ src/pages/Home/containers/Articles.jsx
@@ -9,7 +9,6 @@ const Articles = () => {
       <div className="flex flex-wrap md:gap-x-5 gap-y pb-10">
         <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33% - 21px)]" />
         <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33% - 21px)]" />
-        <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33% - 21px)]" />
       </div>
       <button className="mx-auto flex items-center gap-x-2 font-bold text-primary border-2 border-primary px-6 py-3 rounded-lg  lg:hover:bg-primary hover:bg-white-500  lg:hover:text-white transition-all duration-300">
         <span>More articles</span>
